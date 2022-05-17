5aa208cef1df3bab056ac0c9f7a401f4c53ecea8
Aaron <ken888686@gmail.com>
2022-05-17 17:20:24 +0800

    345. Reverse Vowels of a String

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from typing import List
-
-
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
+    def reverseVowels(self, s: str) -> str:
+        vowels = set('aeiouAEIOU')
+        strList = list(s)
         start = 0
         end = len(s)-1
-        while start <= end:
-            s[start], s[end] = s[end], s[start]
+        while start < end:
+            if s[start] in vowels and s[end] in vowels:
+                strList[start], strList[end] = s[end], s[start]
+            elif s[start] in vowels:
+                end -= 1
+                continue
+            elif s[end] in vowels:
+                start += 1
+                continue
             start, end = start+1, end-1
-        print(s)
+        return ''.join(strList)
 
 
 sol = Solution()
-sol.reverseString(["h", "e", "l", "l", "o"])
+print(sol.reverseVowels("initial"))
