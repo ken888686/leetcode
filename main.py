9f087a5429017b896634e1898a9532e293cbbf02
Aaron <ken888686@gmail.com>
2022-05-18 10:42:00 +0800

    35. Search Insert Position

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ from typing import List
 
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums)-1
+    def searchInsert(self, numberList: List[int], target: int) -> int:
+        left = 0
+        right = len(numberList)-1
         while left <= right:
-            mid = left+(right-left)//2
-            if nums[mid] == target:
+            mid = left + (right - left)//2
+            if numberList[mid] == target:
                 return mid
-            if target > nums[mid]:
-                left = mid+1
+            if numberList[mid] < target:
+                left = mid + 1
             else:
-                right = mid-1
-        return -1
+                right = mid - 1
+        return left
 
 
 sol = Solution()
-print(sol.search([-1, 0, 3, 5, 9, 12], 5))
+print(sol.searchInsert([1, 3, 5, 6], 9))
