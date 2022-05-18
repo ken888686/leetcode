6656281e3ffda8d85e219a1a0b48b84ef95373d1
Aaron <ken888686@gmail.com>
2022-05-18 09:28:03 +0800

    704. Binary Search

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+from typing import List
+
+
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        map = {}
-        result = 0
-        start = 0
-        for i in range(len(s)):
-            if s[i] in map:
-                start = max(map[s[i]]+1, start)
-            result = max(result, i - start + 1)
-            map[s[i]] = i
-        return result
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = left+(right-left)//2
+            if nums[mid] == target:
+                return mid
+            if target > nums[mid]:
+                left = mid+1
+            else:
+                right = mid-1
+        return -1
 
 
 sol = Solution()
-print(sol.lengthOfLongestSubstring("dvdf"))
+print(sol.search([-1, 0, 3, 5, 9, 12], 5))
