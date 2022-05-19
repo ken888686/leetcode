2d94f95ca596bc60033938b14db4c803fd702169
Aaron <ken888686@gmail.com>
2022-05-19 13:30:42 +0800

    557. Reverse Words in a String III

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ from typing import List
 
 
 class Solution:
-    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+    def reverse(self, s: str) -> str:
         left = 0
-        right = len(arr) - 1
+        right = len(s)-1
+        s = list(s)
         while left < right:
-            mid = left + (right - left) // 2
-            if arr[mid] > arr[mid + 1]:
-                right = mid
-            else:
-                left = mid + 1
-        return left
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        return ''.join(s)
+
+    def reverseWords(self, s: str) -> str:
+        s = s.split(' ')
+        for i in range(len(s)):
+            s[i] = self.reverse(s[i])
+        return ' '.join(s)
 
 
 sol = Solution()
-print(sol.peakIndexInMountainArray([0, 1, 2, 3, 5, 6, 0]))
+print(sol.reverseWords('Let\'s take LeetCode contest'))
