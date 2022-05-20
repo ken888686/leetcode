704f5418c9cf08fe8b5d425aa67deaa632540255
Aaron <ken888686@gmail.com>
2022-05-20 16:09:02 +0800

    509. Fibonacci Number

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 class Solution:
-    def isPerfectSquare(self, num: int) -> bool:
-        left = 1
-        right = num
-        while left <= right:
-            mid = left+(right-left)//2
-            if num % mid == 0 and num//mid == mid:
-                return True
-            if (num/mid) < mid:
-                right = mid-1
-            else:
-                left = mid+1
-        return False
+    fib_cache = [0]*31
+
+    def fib_DP_TopDown(self, n: int) -> int:
+        if n <= 1:
+            return n
+        if self.fib_cache[n] != 0:
+            return self.fib_cache[n]
+        else:
+            self.fib_cache[n] = \
+                self.fib_DP_TopDown(n-1) + self.fib_DP_TopDown(n-2)
+        return self.fib_cache[n]
+
+    def fib_DP_BottomUp(self, n: int) -> int:
+        if n <= 1:
+            return n
+        self.fib_cache[1] = 1
+        for i in range(2, n+1):
+            self.fib_cache[i] = \
+                self.fib_cache[i-1] + self.fib_cache[i-2]
+        return self.fib_cache[n]
 
 
 sol = Solution()
-print(sol.isPerfectSquare(1))
+print(sol.fib_DP_TopDown(5))
+print(sol.fib_DP_BottomUp(5))
