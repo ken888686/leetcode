1a77bc130dd78a20ddaf6ef10343b634c7baba59
Aaron <ken888686@gmail.com>
2022-05-20 11:31:02 +0800

    876. Middle of the Linked List

--- main.py
+++ main.py
@@ -1,46 +1,22 @@
-from typing import List, Optional
+from typing import Optional
 
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+class ListNode:
+    def __init__(self, val=0, next=None):
         self.val: int = val
-        self.left: Optional[TreeNode] = left
-        self.right: Optional[TreeNode] = right
+        self.next: Optional[ListNode] = next
 
 
 class Solution:
-    def isSymmetricByWhileLoop(self, root: Optional[TreeNode]) -> bool:
-        stack: List[TreeNode] = []
-        stack.append(root.right)
-        stack.append(root.left)
-        while len(stack) > 0:
-            t1 = stack.pop()
-            t2 = stack.pop()
-            if t1 == None and t2 == None:
-                continue
-            if t1 == None or t2 == None or t1.val != t2.val:
-                return False
-            stack.append(t1.left)
-            stack.append(t2.right)
-            stack.append(t1.right)
-            stack.append(t2.left)
-        return True
-
-    def isSymmetricByRecursive(self, root: Optional[TreeNode]) -> bool:
-        return self.checkSymmetric(root.left, root.right)
-
-    def checkSymmetric(self, tree1: Optional[TreeNode], tree2: Optional[TreeNode]) -> bool:
-        if tree1 == None and tree2 == None:
-            return True
-        if tree1 == None or tree2 == None or tree1.val != tree2.val:
-            return False
-        return self.checkSymmetric(tree1.left, tree2.right) and self.checkSymmetric(tree1.right, tree2.left)
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
 
 
 sol = Solution()
-myTree = TreeNode(1,
-                  TreeNode(2, TreeNode(3), None),
-                  TreeNode(2, None, TreeNode(3)))
-# print(sol.isSymmetricByWhileLoop(myTree))
-print(sol.isSymmetricByRecursive(myTree))
-print(sol.isSymmetricByRecursive())
+head = ListNode(1, ListNode(2, ListNode(
+    3, ListNode(4, ListNode(5)))))
+print(sol.middleNode(head))
