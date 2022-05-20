0d07f7c8c93bd189a3503e9eef60c8aa2897aaa2
Aaron <ken888686@gmail.com>
2022-05-20 14:46:49 +0800

    19. Remove Nth Node From End of List

--- main.py
+++ main.py
@@ -8,15 +8,21 @@ class ListNode:
 
 
 class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = fast = head
-        while fast != None and fast.next != None:
-            slow = slow.next
-            fast = fast.next.next
-        return slow
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        left: Optional[ListNode] = head
+        right: Optional[ListNode] = head
+        for _ in range(n):
+            right = right.next
+        if right == None:
+            return head.next
+        while right.next:
+            left, right = left.next, right.next
+        left.next = left.next.next
+        return head
 
 
 sol = Solution()
-head = ListNode(1, ListNode(2, ListNode(
-    3, ListNode(4, ListNode(5)))))
-print(sol.middleNode(head))
+# head = ListNode(1, ListNode(2, ListNode(
+#     3, ListNode(4, ListNode(5)))))
+head = ListNode(1)
+print(sol.removeNthFromEnd(head, 1))
