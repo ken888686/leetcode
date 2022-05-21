b33df9e780fd5c849c6d06ecddc683b64bd3f198
Aaron <ken888686@gmail.com>
2022-05-21 23:51:43 +0800

    350. Intersection of Two Arrays

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
+import collections
 from typing import List
 
 
 class Solution:
-    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        left = 0
-        right = len(letters)
-        while left < right:
-            mid = left + (right - left)//2
-            if ord(letters[mid]) <= ord(target):
-                left = mid + 1
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        t1 = t2 = 0
+        nums1.sort()
+        nums2.sort()
+        result = []
+        while (t1 < len(nums1)) and (t2 < len(nums2)):
+            if nums1[t1] == nums2[t2]:
+                result.append(nums1[t1])
+                t1 += 1
+                t2 += 1
+            elif nums1[t1] > nums2[t2]:
+                t2 += 1
             else:
-                right = mid
-        return letters[left % len(letters)]
+                t1 += 1
+        return result
+
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums1) > len(nums2):
+            return self.intersection(nums2, nums1)
+        nums2.sort()
+
+        def binarySearch(number: int, numberList: List[int]):
+            left, right = 0, len(numberList)-1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if numberList[mid] == number:
+                    return True
+                if numberList[mid] > number:
+                    right = mid-1
+                else:
+                    left = mid+1
+            return False
+
+        result = set()
+        for i in nums1:
+            if binarySearch(i, nums2):
+                result.add(i)
+        return list(result)
 
 
 sol = Solution()
-ans = sol.nextGreatestLetter(
-    letters=["c", "d", "f", "g", "i", "j"],
-    target="j")
+ans = sol.intersection(
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+)
 print(ans)
