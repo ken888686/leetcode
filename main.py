87572e245432a5392ebe36f924f59ed2bd650685
Aaron <ken888686@gmail.com>
2022-05-22 00:17:40 +0800

    121. Best Time to Buy and Sell Stock

--- main.py
+++ main.py
@@ -3,49 +3,18 @@ from typing import List
 
 
 class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        t1 = t2 = 0
-        nums1.sort()
-        nums2.sort()
-        result = []
-        while (t1 < len(nums1)) and (t2 < len(nums2)):
-            if nums1[t1] == nums2[t2]:
-                result.append(nums1[t1])
-                t1 += 1
-                t2 += 1
-            elif nums1[t1] > nums2[t2]:
-                t2 += 1
+    def maxProfit(self, prices: List[int]) -> int:
+        left, right = 0, 1
+        current = 0
+        while right < len(prices):
+            if prices[right] > prices[left]:
+                current = max(current, prices[right]-prices[left])
             else:
-                t1 += 1
-        return result
-
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        if len(nums1) > len(nums2):
-            return self.intersection(nums2, nums1)
-        nums2.sort()
-
-        def binarySearch(number: int, numberList: List[int]):
-            left, right = 0, len(numberList)-1
-            while left <= right:
-                mid = left + (right - left) // 2
-                if numberList[mid] == number:
-                    return True
-                if numberList[mid] > number:
-                    right = mid-1
-                else:
-                    left = mid+1
-            return False
-
-        result = set()
-        for i in nums1:
-            if binarySearch(i, nums2):
-                result.add(i)
-        return list(result)
+                left = right
+            right += 1
+        return current
 
 
 sol = Solution()
-ans = sol.intersection(
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-)
+ans = sol.maxProfit([7, 5, 3, 1, 7, 1, 8, 6, 9])
 print(ans)
