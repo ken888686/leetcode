7309cfe4be337c1d77555e9d6022cc204fa8900e
Aaron <ken888686@gmail.com>
2022-05-23 16:32:06 +0800

    1254. Number of Closed Islands

--- main.py
+++ main.py
@@ -1,64 +1,39 @@
-from collections import deque
 from typing import List
 
 
 class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if len(grid) == 0:
-            return 0
-        maxLand = 0
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        ans = 0
         for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == 1:
-                    maxLand = max(maxLand, self.dfs(grid, i, j))
-        return maxLand
+            for j in range(len(grid[0])):
+                if grid[i][j] == 0:
+                    ans += 1 if self.dfs(grid, i, j) else 0
+        return ans
 
-    def dfs(self, grid: List[List[int]], gr: int, gc: int) -> int:
-        if gr < 0 or gr >= len(grid) or gc < 0 or gc >= len(grid[gr]) or grid[gr][gc] != 1:
-            return 0
-        grid[gr][gc] = 0
-        return 1 \
-            + self.dfs(grid, gr+1, gc) \
-            + self.dfs(grid, gr-1, gc) \
-            + self.dfs(grid, gr, gc+1) \
-            + self.dfs(grid, gr, gc-1)
-
-    def bfs(self, grid: List[List[int]]) -> int:
-        if len(grid) == 0:
-            return 0
-
-        gr, gc = len(grid), len(grid[0])
-        queue, max_area = deque([]), 0
-
-        def _bfs(queue: deque) -> int:
-            cur_area = 1
-            while queue:
-                i, j = queue.popleft()
-                for ni, nj in [[i+1, j], [i-1, j], [i, j+1], [i, j-1]]:
-                    if 0 <= ni < gr and 0 <= nj < gc and grid[ni][nj] == 1:
-                        grid[ni][nj] = 0
-                        cur_area += 1
-                        queue.append((ni, nj))
-            return cur_area
-
-        for i in range(gr):
-            for j in range(gc):
-                if grid[i][j] == 1:
-                    grid[i][j] = 0
-                    queue.append([i, j])
-                    max_area = max(max_area, _bfs(queue))
-        return max_area
+    def dfs(self, grid: List[List[int]], row: int, col: int) -> bool:
+        if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]):
+            return False
+        if grid[row][col] == 1:
+            return True
+        grid[row][col] = 1
+        d1 = self.dfs(grid, row+1, col)
+        d2 = self.dfs(grid, row-1, col)
+        d3 = self.dfs(grid, row, col+1)
+        d4 = self.dfs(grid, row, col-1)
+        return d1 and d2 and d3 and d4
 
 
 sol = Solution()
-ans = sol.bfs([
-    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-    [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
-    [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
+ans = sol.closedIsland([
+    [0, 0, 1, 1, 0, 1, 0, 0, 1, 0],
+    [1, 1, 0, 1, 1, 0, 1, 1, 1, 0],
+    [1, 0, 1, 1, 1, 0, 0, 1, 1, 0],
+    [0, 1, 1, 0, 0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1, 1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 0, 1, 1, 0, 1, 1, 0]
 ])
 print(ans)
