cef17f7aaf333c544264673c7a07313f26a67eae
Aaron <ken888686@gmail.com>
2022-05-23 10:06:57 +0800

    200. Number of Islands

--- main.py
+++ main.py
@@ -2,26 +2,32 @@ from typing import List
 
 
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        if image[sr][sc] == newColor:
-            return image
-        self.fill(image, sr, sc, image[sr][sc], newColor)
-        return image
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if len(grid) == 0:
+            return 0
+        island = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == "1":
+                    island += 1
+                    self.check(grid, i, j)
+        return island
 
-    def fill(self, image: List[List[int]], sr: int, sc: int, color: int, newColor: int) -> None:
-        if sr < 0 or sr >= len(image) or sc < 0 or sc >= len(image[sr]) or image[sr][sc] != color:
+    def check(self, grid: List[List[str]], gr: int, gc: int) -> None:
+        if gr < 0 or gr >= len(grid) or gc < 0 or gc >= len(grid[gr]) or grid[gr][gc] != "1":
             return
-        image[sr][sc] = newColor
-        self.fill(image, sr+1, sc, color, newColor)
-        self.fill(image, sr, sc+1, color, newColor)
-        self.fill(image, sr-1, sc, color, newColor)
-        self.fill(image, sr, sc-1, color, newColor)
+        grid[gr][gc] = "0"
+        self.check(grid, gr+1, gc)
+        self.check(grid, gr-1, gc)
+        self.check(grid, gr, gc+1)
+        self.check(grid, gr, gc-1)
 
 
 sol = Solution()
-ans = sol.floodFill(
-    image=[[1, 1, 1], [1, 1, 0], [1, 0, 1]],
-    sr=1,
-    sc=1,
-    newColor=2)
+ans = sol.numIslands([
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"]
+])
 print(ans)
