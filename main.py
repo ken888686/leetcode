30f0ca496a5d68f51a0e89f0987ac711db9b2a92
Aaron <ken888686@gmail.com>
2022-05-24 13:40:06 +0800

    34. Find First and Last Position of Element in Sorted Array

--- main.py
+++ main.py
@@ -2,37 +2,39 @@ from typing import List
 
 
 class Solution:
-    def numEnclaves(self, grid: List[List[int]]) -> int:
-        ans = 0
-        row = len(grid)
-        col = len(grid[0])
-        if not row or not col:
-            return 0
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) < 1:
+            return [-1, -1]
+        first = self.firstPosition(nums, target)
+        last = self.lastPosition(nums, target)
+        return [first, last]
 
-        for i in range(row):
-            for j in range(col):
-                if i == 0 or i == row-1 or j == 0 or j == col-1:
-                    self.dfs(grid, i, j)
+    def firstPosition(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = left + (right - left)//2
+            if nums[mid] >= target:
+                right = mid
+            else:
+                left = mid+1
+        return left if nums[left] == target else -1
 
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] == 1:
-                    ans += 1
-        return ans
-
-    def dfs(self, grid: List[List[int]], row: int, col: int) -> int:
-        if 0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col]:
-            grid[row][col] = 0
-            for i, j in [[row+1, col], [row-1, col], [row, col+1], [row, col-1]]:
-                self.dfs(grid, i, j)
+    def lastPosition(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = left + (right - left + 1)//2
+            if nums[mid] <= target:
+                left = mid
+            else:
+                right = mid-1
+        return left if nums[right] == target else -1
 
 
 sol = Solution()
-grid = [
-    [0, 0, 0, 0],
-    [1, 1, 0, 0],
-    [0, 0, 1, 0],
-    [0, 0, 0, 1]
-]
-ans = sol.numEnclaves(grid)
+ans = sol.searchRange(
+    [5, 5, 5, 5, 5,
+     7, 7, 7, 8, 8,
+     8, 8, 9, 9, 9,
+     9, 9, 9, 9, 9],
+    6)
 print(ans)
