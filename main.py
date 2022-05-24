f98143b8e9a469023975a36a4add274d2bd61a32
Aaron <ken888686@gmail.com>
2022-05-24 23:40:52 +0800

    332. Reconstruct Itinerary

--- main.py
+++ main.py
@@ -2,19 +2,57 @@ from typing import List
 
 
 class Solution:
-    def hIndex(self, citations: List[int]) -> int:
-        length = len(citations)
-        paper = [0] * (length + 1)
-        for c in citations:
-            paper[min(length, c)] += 1
-        s, h = paper[-1], length
-        while h > s:
-            h -= 1
-            s += paper[h]
-        return h
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        map: dict[str, List[str]] = {}
+        route: List[str] = []
+        # 建立地圖
+        for s, d in tickets:
+            if s in map:
+                map[s].append(d)
+            else:
+                map[s] = [d]
+        # 機場字母排序
+        for i in map:
+            map[i].sort()
+
+        def visit(airport: str):
+            # 若無下一個飛行目的地
+            if airport not in map:
+                route.append(airport)
+                return
+            # 取得該地點所有目的地
+            destinations = map[airport]
+            # 檢查該地點所有目的地
+            while destinations:
+                # 由字母小的為優先目的地
+                dest = destinations.pop(0)
+                visit(dest)
+            # 到達此地
+            route.append(airport)
+        visit("JFK")
+
+        route.reverse()
+        return route
 
 
 sol = Solution()
-data = [1, 3, 2]
-ans = sol.hIndex(data)
+tickets = [
+    ["JFK", "SFO"],
+    ["JFK", "ATL"],
+    ["SFO", "ATL"],
+    ["ATL", "JFK"],
+    ["ATL", "SFO"],
+    ["JFK", "ABC"],
+    ["ABC", "SFO"]
+]
+ans = sol.findItinerary(tickets)
+print(ans)
+
+tickets = [
+    ["JFK", "MUC"],
+    ["MUC", "LHR"],
+    ["LHR", "SFO"],
+    ["SFO", "SJC"]
+]
+ans = sol.findItinerary(tickets)
 print(ans)
