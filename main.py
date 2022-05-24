549fb47af02705a7c787d6a72af34ecf48ef863f
Aaron <ken888686@gmail.com>
2022-05-24 20:40:53 +0800

    275. H-Index II

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ from typing import List
 
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort()
-        counter = 1
-        for i in range(len(citations)-1, -1, -1):
-            if citations[i] >= counter:
-                counter += 1
-        return counter - 1
+        left, right, length = 0, len(citations)-1, len(citations)
+        while left <= right:
+            mid = left + (right - left) // 2
+            if (length - mid) <= citations[mid]:
+                right = mid-1
+            else:
+                left = mid+1
+        return length - left
 
 
 sol = Solution()
-ans = sol.hIndex([1, 0, 4, 6, 3])
-print(ans)
-ans = sol.hIndex([1, 2, 100])
-print(ans)
-ans = sol.hIndex([1, 1])
-print(ans)
-ans = sol.hIndex([0])
+ans = [
+    sol.hIndex([0, 1, 3, 5, 6, 7]),
+    sol.hIndex([1, 2, 100]),
+    sol.hIndex([1, 1]),
+    sol.hIndex([1]),
+    sol.hIndex([0])
+]
 print(ans)
