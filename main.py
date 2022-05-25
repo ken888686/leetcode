29a9245c4b7bc16ae35dd32ce595134c87c97928
Aaron <ken888686@gmail.com>
2022-05-25 11:53:14 +0800

    139. Word Break

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+from typing import List
+
+
 class Solution:
-    def checkRecord(self, s: str) -> bool:
-        a = l = 0
-        for c in s:
-            if c == "A":
-                a += 1
-            if c == "l":
-                l += 1
-            else:
-                l = 0
-            if a >= 2 or l >= 3:
-                return False
-        return True
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        d = [False] * (len(s) + 1)
+        d[0] = True
+        for i in range(len(s)+1):
+            for j in range(i):
+                if d[j] and s[j:i] in wordDict:
+                    d[i] = True
+                    break
+        return d[len(s)]
 
 
 sol = Solution()
-ans = sol.checkRecord("AA")
+ans = sol.wordBreak(s="applepencil", wordDict=["apple", "pencil"])
+print(ans)
+ans = sol.wordBreak(s="a", wordDict=["a"])
 print(ans)
