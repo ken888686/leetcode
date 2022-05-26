8850dba70076009c33d6470ac423fe222deedd85
Aaron <ken888686@gmail.com>
2022-05-26 11:27:36 +0800

    191. Number of 1 Bits

--- main.py
+++ main.py
@@ -2,18 +2,14 @@ from typing import List
 
 
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        result: list[list[int]] = []
-        for i in range(numRows):
-            result.append([])
-            for j in range(i + 1):
-                if j in (0, i):
-                    result[i].append(1)
-                else:
-                    result[i].append(result[i-1][j-1] + result[i-1][j])
-        return result
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n != 0:
+            n &= (n-1)
+            count += 1
+        return count
 
 
 sol = Solution()
-ans = sol.generate(5)
+ans = sol.hammingWeight(int('00001101', 2))
 print(ans)
