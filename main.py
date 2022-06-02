1aff10e5eac0d4c2aca217ed26bc1f9a96d52282
Aaron <ken888686@gmail.com>
2022-06-02 18:18:19 +0800

    136. Single Number

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
+from functools import reduce
 from typing import List
 
 
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        carry = False
-        digits[len(digits) - 1] += 1
-        for i in range(len(digits)-1, -1, -1):
-            currentNum = digits[i] if not carry else digits[i]+1
-            if currentNum > 9:
-                digits[i] = currentNum % 10
-                carry = True
-            else:
-                digits[i] = currentNum
-                carry = False
-                break
-        if carry:
-            digits.insert(0, 1)
-        return digits
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(lambda x, y: x ^ y, nums)
 
 
 sol = Solution()
-ans = sol.plusOne([9, 2, 9])
+ans = sol.singleNumber([1, 1, 2, 2, 3, 3, 4, 4, 5, 6, 6, 7, 8, 9, 7, 8, 9])
 print(ans)
