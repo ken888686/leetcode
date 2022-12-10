f6682cec354aea173e2cc7960c83f42c9f53a793
Aaron <ken888686@gmail.com>
2022-12-10 16:10:04 +0900

    reverse integer

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 "use strict";
-function palindrome(s) {
-    let result = true, left = 0, right = s.length - 1;
-    while (left < right) {
-        if (s[left] !== s[right]) {
-            result = false;
-            break;
-        }
-        left++;
-        right--;
-    }
-    return result;
-    // return s.split("").every((curr, i) => {
-    //   return curr === s[s.length - i - 1];
-    // });
-}
-function main() {
-    console.log(palindrome("aba"));
-}
+const reverseInt = (n) => {
+    return (Number.parseInt(n.toString().split("").reverse().join("")) * Math.sign(n));
+};
+const main = () => {
+    console.log(reverseInt(-124123));
+};
 main();
--- index.ts
+++ index.ts
@@ -1,23 +1,11 @@
-function palindrome(s: string): boolean {
-  let result = true,
-    left = 0,
-    right = s.length - 1;
-  while (left < right) {
-    if (s[left] !== s[right]) {
-      result = false;
-      break;
-    }
-    left++;
-    right--;
-  }
-  return result;
-  // return s.split("").every((curr, i) => {
-  //   return curr === s[s.length - i - 1];
-  // });
-}
+const reverseInt = (n: number): number => {
+  return (
+    Number.parseInt(n.toString().split("").reverse().join("")) * Math.sign(n)
+  );
+};
 
-function main() {
-  console.log(palindrome("aba"));
-}
+const main = () => {
+  console.log(reverseInt(-124123));
+};
 
 main();
