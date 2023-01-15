664e8aed102563e76f50154198ab0580b80cdb3f
Aaron <ken888686@gmail.com>
2023-01-15 22:08:29 +0900

    125. Valid Palindrome

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 "use strict";
-const chunk = (arr, n) => {
-    const result = [], temp = [];
-    for (let x of arr) {
-        if (temp.length === n) {
-            result.push([...temp]);
-            temp.length = 0;
-        }
-        temp.push(x);
-    }
-    result.push(temp);
-    return result;
-};
-const main = () => {
-    console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, 6, 7, 8, 2], 3));
-};
+function solution(s) {
+    let left = 0, right = s.length - 1;
+    s = s.toLowerCase().replace(/[\W_]/g, "");
+}
+function main() {
+    console.log(solution("0p"));
+    console.log(solution(" "));
+}
 main();
--- index.ts
+++ index.ts
@@ -1,19 +1,12 @@
-const chunk = (arr: number[], n: number): number[][] => {
-  const result: number[][] = [],
-    temp: number[] = [];
-  for (let x of arr) {
-    if (temp.length === n) {
-      result.push([...temp]);
-      temp.length = 0;
-    }
-    temp.push(x);
-  }
-  result.push(temp);
-  return result;
-};
+function solution(s: string) {
+  let left = 0,
+    right = s.length - 1;
+  s = s.toLowerCase().replace(/[\W_]/g, "");
+}
 
-const main = () => {
-  console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, 6, 7, 8, 2], 3));
-};
+function main() {
+  console.log(solution("0p"));
+  console.log(solution(" "));
+}
 
 main();
